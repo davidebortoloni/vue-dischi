2e81e9fb517c90a0b175db7008ce2cd7b18f5131
davidebortoloni <davidebortoloni@gmail.com>
2021-09-02 16:51:28 +0200

    Inserimento API con album

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-console.log('Vue', Vue);
-
 Vue.config.devtools = true;
 
 const app = new Vue({
-  el: '#app',
-  data: {},
+  el: "#app",
+  data: {
+    albums: [],
+  },
   methods: {},
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        this.albums = res.data.response;
+      });
+  },
 });

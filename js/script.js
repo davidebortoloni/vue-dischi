b7578792f6040d1fc02ddbab4889786d9f000824
davidebortoloni <davidebortoloni@gmail.com>
2021-09-02 18:08:40 +0200

    Inserimento filter

--- js/script.js
+++ js/script.js
@@ -4,15 +4,36 @@ const app = new Vue({
   el: "#app",
   data: {
     albums: [],
+    genreSelected: "All",
   },
-  methods: {},
-  computed: {
-    sortedAlbums() {
+  methods: {
+    sortAlbums() {
       let sortedAlbums = this.albums;
       sortedAlbums.sort(function (a, b) {
         return a.year - b.year;
       });
       return sortedAlbums;
+    },
+  },
+  computed: {
+    filteredAlbums() {
+      const sortedAlbums = this.sortAlbums();
+      let filteredAlbums = sortedAlbums;
+      if (this.genreSelected !== "All") {
+        filteredAlbums = sortedAlbums.filter(
+          (album) => album.genre === this.genreSelected
+        );
+      }
+      return filteredAlbums;
+    },
+    genres() {
+      const genres = [];
+      this.albums.forEach((album) => {
+        if (!genres.includes(album.genre)) {
+          genres.push(album.genre);
+        }
+      });
+      return genres;
     },
   },
   created() {
